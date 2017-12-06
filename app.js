500c8027e8a373f3b34c13bbc8f13009cc636cb4
unicorn-84 <dev.kapitonov@gmail.com>
2017-12-06 10:29:43 +0300

    nconf

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const config = require('./config');
+
+const app = express();
+app.set('port', config.get('port'));
+
+http.createServer(app).listen(app.get('port'), () => {
+  console.log(`Server listening on port ${app.get('port')}`);
+});
+
+app.use((req, res, next) => {
+  if (req.url === '/') {
+    res.send('<h1>NewsBucket</h1>');
+  } else {
+    next();
+  }
+});
+
+app.use((req, res, next) => {
+  if (req.url === '/test') {
+    res.send('<h3>Test</h3>');
+  } else {
+    next();
+  }
+});
+
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  if (app.get('env') === 'development') {
+    res.status(error.status || 500);
+    res.send(`<h1>${error.message}</h1><h3>${error.status}</h3><h4>${error.stack}</h4>`);
+  }
+});
+
+/*
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -7,11 +47,9 @@ var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./routes/users');*/
 
-var app = express();
-
-// view engine setup
+/*// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -32,22 +70,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+
+
+
+module.exports = app;*/
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+// app.use((req, res, next) => {
+//   const err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+// // error handler
+// app.use((err, req, res, next) => {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.send(`${res.locals.message}\n${res.locals.error}`);
+// });
