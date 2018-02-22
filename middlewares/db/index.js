9451610408e907cc7d4f8b74e345b52096359156
unicorn-84 <dev.kapitonov@gmail.com>
2018-02-22 18:30:37 +0300

    2.0.0

---
+++ middlewares/db/index.js
@@ -0,0 +1,16 @@
+const database = require('./database');
+
+module.exports = (request, response, next) => {
+  database.connectToDb((error, result) => {
+    if (error) {
+      next(error);
+    }
+    let massMedia = [];
+    result.forEach((item) => {
+      massMedia = massMedia.concat(item.news);
+    });
+    massMedia.sort((a, b) => a.id - b.id);
+    request.specialData = massMedia;
+    next();
+  });
+};
